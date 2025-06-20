a1f543a95672369adad900ede5d3755caedee525
mukhokironmoy <mukhokironmoy@gmail.com>
2025-06-20 19:22:54 +0530

    feat: added login form and POST handler with basic validation and feedback

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, url_for, render_template
-
+from flask import Flask, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,6 +12,18 @@ def hello_fxn():
 @app.route('/user/<name>')
 def greet(name):
     return render_template('hello.html', name=name)
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    elif request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == "yomnorik" and password == "pw":
+            return render_template('hello.html', name=username, password=password)
+        else:
+            return render_template('login.html', caution = "Username or Password is incorrect. Please try again." )
 
 if __name__ == "__main__":
     app.run(debug=True)
