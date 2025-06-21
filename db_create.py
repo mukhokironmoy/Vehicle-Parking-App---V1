7306926a494f364a0a5017310cf2ce3f84cbb216
mukhokironmoy <mukhokironmoy@gmail.com>
2025-06-21 19:49:43 +0530

    feat: added ParkingLot model with required fields; registered in db_create script

--- db_create.py
+++ db_create.py
@@ -1,5 +1,6 @@
 from app import app,db
 from models.user import User
+from models.parking_lot import ParkingLot
 
 with app.app_context():
-    db.create_all()
+    db.create_all() 
---
+++ models/parking_lot.py
@@ -0,0 +1,9 @@
+from extensions import db
+
+class ParkingLot(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    prime_location_name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(300), nullable = False)
+    pin_code = db.Column(db.String(10), nullable = False)
+    price_per_hour = db.Column(db.Float, nullable = False)
+    max_spots = db.Column(db.Integer, nullable = False)
